bab00d4b4b4f3521c46f306a378b654705d63ba1
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-03-17 11:26:39 +0500

    kjd

--- dist/brand-model/dto/update-brand-model.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateBrandModelDto } from './create-brand-model.dto';
-declare const UpdateBrandModelDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateBrandModelDto>>;
-export declare class UpdateBrandModelDto extends UpdateBrandModelDto_base {
-}
-export {};
--- dist/call/entities/call.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Call {
-    id: number;
-    name: string;
-    phone: string;
-    date: Date;
-    createdAt: Date;
-    updatedAt: Date;
-}
