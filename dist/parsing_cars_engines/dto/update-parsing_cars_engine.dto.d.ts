27860800fd083982f55c02398fa3406c981e5dc0
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-04-07 23:34:32 +0500

    changed

--- dist/parsing_cars_engines/dto/update-parsing_cars_engine.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateParsingCarsEngineDto } from './create-parsing_cars_engine.dto';
-declare const UpdateParsingCarsEngineDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateParsingCarsEngineDto>>;
-export declare class UpdateParsingCarsEngineDto extends UpdateParsingCarsEngineDto_base {
-}
-export {};
