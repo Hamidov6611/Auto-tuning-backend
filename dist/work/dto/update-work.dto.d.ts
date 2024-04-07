206b50e5324fb2d6ce341f4aab9d33c9b5e08eb1
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-04-07 22:56:47 +0500

    changed

--- dist/work/dto/update-work.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateWorkDto } from './create-work.dto';
-declare const UpdateWorkDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateWorkDto>>;
-export declare class UpdateWorkDto extends UpdateWorkDto_base {
-}
-export {};
--- dist/work/entities/work.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Work {
-    id: number;
-    title: string;
-    description: string;
-    img: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
